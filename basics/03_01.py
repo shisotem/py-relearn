c5a2b93c0ce70e4bacdcbd58270f12091d24d621
shisotem <129283841+shisotem@users.noreply.github.com>
2024-01-23 21:35:22 +0900

    feat: add 03_01

---
+++ basics/03_01.py
@@ -0,0 +1,23 @@
+"""
+This
+is
+a
+multi-line
+comment
+"""
+
+
+# Apple value（上に書くのが良いとされる）
+some_value = 100
+
+
+# \: 現在翻訳している行がまだ次の行に続いていることを示す
+# s = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' \
+#     + 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+s = (
+    'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+    + 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+)
+print(s)
+# x = (1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+#      + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1)
