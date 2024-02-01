d1799c2b4ff93c18a1f9e5a1d496288618619b71
shisotem <129283841+shisotem@users.noreply.github.com>
2024-02-02 02:04:11 +0900

    feat: add 07_04

--- basics/07_file_operations_and_systems/07_04.py
+++ basics/07_file_operations_and_systems/07_04.py
@@ -41,23 +41,100 @@ os.rmdir(temp_dir)
 """
 subprocess
 """
-import subprocess
-
-subprocess.run(['ls', '-a'])  # . .. .git .gitignore README.md applications basics
-
-# Legacyで非推奨 (-> subprocessを使う)
+# os.system() は、Legacyで非推奨 (-> subprocessを使う)
 # os.system('ls -a')  # . .. .git .gitignore README.md applications basics
 
-# 文字列でコマンドを実行可能
-# 注: シェルインジェクションの危険性 e.g. $ rm -rf /
-subprocess.run('ls -a | grep *.md', shell=True)  # README.md
+import subprocess
 
+# リスト形式でコマンドを実行
+subprocess.run(['ls', '-a'])  # . .. .git .gitignore README.md applications basics
+
+# # ちゃんと実行中断される!
+# subprocess.run(['hoge'])  # FileNotFoundError: [Errno 2] No such file or directory: 'hoge'
+
+
+# 文字列形式でコマンドを実行(shell=True)
+# 注: シェルインジェクションの危険性あり e.g. $ rm -rf /
+subprocess.run('ls -a | grep .*\.md$', shell=True)  # README.md
 
 r = subprocess.run('hoge', shell=True)  # /bin/sh: hoge: command not found
-print(f'r.returncode: {r.returncode} (このように0以外の場合は異常終了)')
-print('shell=True -> 異常終了でも処理が止まらず、引き続き実行されてしまう!')
+print(f'r.returncode: {r.returncode} (このようにリターンコードが0以外の場合は異常終了)')
+print('shell=True の場合、異常終了なのに処理が止まらず、引き続き実行されてしまう!')
+
+# # なので、Exceptionをraiseするような処理を自分で書いたり、
+# if r.returncode != 0:
+#     raise Exception(f"'hoge' command failed with return code {r.returncode}")
+
+# # あるいは、check=Trueを指定する
+# subprocess.run(
+#     'hoge', shell=True, check=True
+# )  # subprocess.CalledProcessError: Command 'hoge' returned non-zero exit status 127.
+
+
+# Popenを使えば、リスト形式でもパイプライン処理は可能
+p1 = subprocess.Popen(['ls', '-a'], stdout=subprocess.PIPE)
+p2 = subprocess.Popen(['grep', '.*\.md$'], stdin=p1.stdout, stdout=subprocess.PIPE)
+p1.stdout.close()
+output = p2.communicate()[0]
+print(output)  # b'README.md\n'
 
 
 """
-datetime・バックアップファイル
+datetime・time (バックアップファイルの作成)
 """
+import datetime
+
+now = datetime.datetime.now()
+print(now)  # 2024-02-02 00:36:28.572636
+print(now.isoformat())  # 2024-02-02T00:36:28.572636
+print(
+    now.strftime('%Y / %m / %d ( %H : %M : %S . %f )')
+)  # 2024 / 02 / 02 ( 00 : 36 : 28 . 572636 )
+
+today = datetime.date.today()
+print(today)  # 2024-02-02
+print(today.isoformat())  # 2024-02-02
+print(today.strftime('%Y / %m / %d'))  # 2024 / 02 / 02
+
+# 任意の時刻を作成
+t = datetime.time(hour=1, minute=10, second=5, microsecond=100)
+print(t)  # 01:10:05.000100
+print(t.isoformat())  # 01:10:05.000100
+print(t.strftime('%H / %M / %S . %f'))  # 01 / 10 / 05 . 000100
+
+# 時刻の計算
+now = datetime.datetime.now()
+print(now)  # 2024-02-02 00:36:28.572636
+
+delta_neg = datetime.timedelta(weeks=-1)
+print(now + delta_neg)  # 2024-01-26 00:36:28.572636
+delta_pos = datetime.timedelta(weeks=1)
+print(now - delta_pos)  # 2024-01-26 00:36:28.572636
+
+d = datetime.timedelta(days=365)
+print(now + d)  # 2025-02-01 00:36:28.572636
+
+
+import time
+
+print('## start')
+time.sleep(2)
+print('## end')
+
+# UNIX時間(エポックタイム): 1970/01/01 00:00:00 (= epoch) からの経過秒数
+print(time.time())  # 1706803028.724313
+
+
+# バックアップファイルの作成
+import shutil
+
+now = datetime.datetime.now()
+file_name = 'backup_test.txt'
+
+# バックアップを取っておく
+if os.path.exists(file_name):
+    shutil.copy(file_name, f'{file_name}.{now.strftime("%Y_%m_%d_%H_%M_%S")}')
+
+# オリジナルに変更を加える
+with open(file_name, 'a') as f:
+    f.write('This is a backup test file.\n')
