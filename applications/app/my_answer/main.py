a11d0f1da82a418382427959dd0c402868748599
shisotem <129283841+shisotem@users.noreply.github.com>
2024-02-06 19:16:52 +0900

    refactor: refactor with instructor code and add my comments

--- applications/app/my_answer/main.py
+++ applications/app/my_answer/main.py
@@ -1,23 +1,2 @@
-import os
-
-from roboter import recommendation
-from roboter import restaurant_data_manager
-from roboter import utils
-
-
-user_name = input('< Hello, I am Roboko. What is your name?\n> ')
-
-if utils.is_data_present('ranking.csv'):
-    next_recommendation_index = 0
-    while next_recommendation_index >= 0:
-        recommendation.recommend_restaurant(next_recommendation_index)
-        if recommendation.is_preferred():
-            break
-        next_recommendation_index = recommendation.update_next_recommendation_index(
-            next_recommendation_index
-        )
-
-new_restaurant = input(f'< {user_name}, which restaurants do you like?\n> ')
-restaurant_data_manager.add_restaurant_vote(new_restaurant)
-restaurant_data_manager.sort_restaurant()
-print(f'> Thank you so much, {user_name}!\n> Have a good day!')
+import roboter.controller.conversation
+roboter.controller.conversation.talk_about_restaurant()
