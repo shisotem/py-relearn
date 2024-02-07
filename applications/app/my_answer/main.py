238ad318f58d2f974474ccb93b5a2a66d57acbc7
shisotem <129283841+shisotem@users.noreply.github.com>
2024-02-07 12:22:53 +0900

    refactor: add main function

--- applications/app/my_answer/main.py
+++ applications/app/my_answer/main.py
@@ -1,2 +1,10 @@
 import roboter.controller.conversation
-roboter.controller.conversation.talk_about_restaurant()
+
+
+def main():
+    roboter.controller.conversation.talk_about_restaurant()
+
+
+# メインで実行するスクリプトは、main関数を使って書くべき -> 最初に呼び出す処理だとわかりやすい
+if __name__ == '__main__':
+    main()
